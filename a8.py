1febfc4e4a005efa7b9c0096e80c1a628ea41101
Kevin Asante <koasante@cps.edu>
2025-04-25 10:49:07 -0500

    a8.py Completed

--- a8.py
+++ a8.py
@@ -1,18 +1,19 @@
 from neural import *
 
-# print("\n\nTraining XOR\n\n")
-# xor_training_data = [
-#     ([1, 1], [0]), 
-#     ([1, 0], [1]), 
-#     ([0, 1], [1]), 
-#     ([0, 0], [0])
-#     ]
+print("\n\nTraining XOR\n\n")
+xor_training_data = [
+    ([1, 1], [0]), 
+    ([1, 0], [1]), 
+    ([0, 1], [1]), 
+    ([0, 0], [0])
+    ]
 
-# xorn = NeuralNet(2, 1, 1)
-# xorn.train(xor_training_data, learning_rate= .5)
-# print(xorn.test_with_expected(xor_training_data))
+# (Input, Hidden Nods ,Output)
+xorn = NeuralNet(2, 1, 1)
+
+xorn.train(xor_training_data, learning_rate= .5)
+
+print(xorn.test_with_expected(xor_training_data))
 
 
-# print("<<<<<<<<<<<<<< XOR >>>>>>>>>>>>>>\n")
-
-
+print("<<<<<<<<<<<<<< XOR >>>>>>>>>>>>>>\n")
